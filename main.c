b6033abdda7b1c1795cad4a55e38d86e9cc9a1c5
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-08-08 16:12:13 -0300

    Trying

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
 
 int main(){
     printf("======================================\n");
-    printf("=             Bem vindo              =\n");
+    printf("=             Bem vindo              =\n"); 
     printf("======================================\n");
-    
-    int tentativas = 3;
-    while(tentativas > 0){
-        std::cout << "Adivinhe o Número: ";
-        tentativas--;
+    int random_number = rand() % 10 + 1;
+    int chute;
+    while(chute != random_number){
+        std::cout << "Adivinhe o Numero: ";
+        std::cin  >> chute;
+        printf("--------------------------------------\n");
+        if(chute == random_number){
+            printf("-           Perfeito, Ganhou!        -\n"); 
+            printf("--------------------------------------\n");
+        }else{           
+            if(chute > random_number){
+                printf("-           Chute muito alto         -\n"); 
+            }else{
+                printf("-           Chute muito baixo        -\n"); 
+
+            }
+            printf("--------------------------------------\n");
+            tentativas--;
+        }
     }
+    std::cout << random_number;
 }
